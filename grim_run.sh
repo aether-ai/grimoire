e9b37504cfb9ff42e8fa55614c474a6c2f40c71a
Banjo Obayomi <banjtheman@gmail.com>
2020-04-15 19:40:35 -0400

    update run command for demo

--- grim_run.sh
+++ grim_run.sh
@@ -11,6 +11,6 @@ mkdir -p data_files/audio
 mkdir -p data_files/images
 mkdir -p data_files/text
 echo "Starting Grimoire"
-docker run -p 8501:8501 -p 9000:9000 -p 9005:80 -d -v $(pwd)/data_files/:/home/data_files/ -v $(pwd)/models/:/home/models/ -v $(pwd)/grimoire/:/home/grimoire/ -v $(pwd)/spells/:/home/spells/ --rm aetheraidocker/grimoire:latest
+docker run -p 8501:8501 -p 9000:9000 -p 9005:80 -d -v $(pwd)/data_files/:/home/data_files/ -v $(pwd)/models/:/home/models/ -v $(pwd)/grimoire/:/home/grimoire/ -v $(pwd)/spells/:/home/spells/ --rm aetheraidocker/grimoire:demo_site
 cd -
 echo "Grimoire is up at http://localhost:9005/"
