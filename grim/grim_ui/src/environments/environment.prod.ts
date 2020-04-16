a9b46b15cc67b39f6b0a9ec5e79b1d176d76531c
Banjo Obayomi <banjtheman@gmail.com>
2020-04-16 17:49:11 -0400

    new demo ip

--- grim/grim_ui/src/environments/environment.prod.ts
+++ grim/grim_ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  ui_url: "http://3.95.182.1:9005",
-  api_url: "http://3.95.182.1:9000",
-  streamlit_url: "http://3.95.182.1:8501"
+  ui_url: "http://34.203.190.228:9005",
+  api_url: "http://34.203.190.228:9000",
+  streamlit_url: "http://34.203.190.228:8501"
 };
